745550cb4ab90c3f9f897cafa8ade8c0ba164d42
Juan Martin Nazarian <jmnazarian@gmail.com>
2025-08-20 13:17:02 -0300

    check the height before dividing

--- TP0/alumno_mas_bajo.py
+++ TP0/alumno_mas_bajo.py
@@ -6,13 +6,13 @@ class Alumno:
 # 1.2, 1.15, 1.14, 1.12, 1.02, 0.98, 1.18, 1.23
 alumnos = [
   Alumno(nombre="alumno 1", altura=1.2),
-  Alumno(nombre="alumno 2", altura=1.02),
-  Alumno(nombre="alumno 3", altura=1.23),
   Alumno(nombre="alumno 4", altura=1.15),
-  Alumno(nombre="alumno 5", altura=0.98),
-  Alumno(nombre="alumno 6", altura=1.12),
   Alumno(nombre="alumno 7", altura=1.14),
+  Alumno(nombre="alumno 6", altura=1.12),
+  Alumno(nombre="alumno 2", altura=1.02),
+  Alumno(nombre="alumno 5", altura=0.98),
   Alumno(nombre="alumno 8", altura=1.18),
+  Alumno(nombre="alumno 3", altura=1.23),
 ]
 
 
@@ -21,16 +21,18 @@ def binary_search(vector, init, end):
     return end
   else:
     middle = (end + init) // 2
-    left = binary_search(vector, init, middle)
-    right = binary_search(vector, middle + 1, end)
-    # print(f"left: {left} - right: {right}")
-    return left if vector[left].altura < vector[right].altura else right
+    if vector[middle].altura < vector[middle + 1].altura:
+      left = binary_search(vector, init, middle)
+      return left
+    else:
+      right = binary_search(vector, middle + 1, end)
+      return right
 
 
 
 def indice_mas_bajo(alumnos):                   
     min_height_i = binary_search(alumnos, 0, len(alumnos) - 1)
-    # print(f"Altura mas baja: {min_height}")
+    # print(f"Altura mas baja: {min_height_i}")
     return min_height_i
 
 def validar_mas_bajo(alumnos, indice):
